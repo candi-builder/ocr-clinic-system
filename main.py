2fa6258e6de5c7397a03387cdf42834963cf326b
basstimam <basstimam@gmail.com>
2024-04-25 07:21:39 +0700

    changing from flask to fastapi

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, File, UploadFile
 import easyocr
 import os
-from werkzeug.utils import secure_filename
-import imghdr
 
-app = Flask(__name__)
-reader = easyocr.Reader(['id'])
+from fastapi.responses import JSONResponse
 
+app = FastAPI()
+reader = easyocr.Reader(['id'], gpu=False)
 ALLOWED_EXTENSIONS = {'jpg', 'png'}
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/scan', methods=['POST'])
-def scan_image():
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file_ext = filename.rsplit('.', 1)[1].lower()
-        file_name = f'image.{file_ext}'
-        file.save(file_name)
-        result = reader.readtext(file_name, detail=0)
-        os.remove(file_name)
-        return jsonify({'message': result})
-    else:
-        return jsonify({'error': 'No file provided or invalid file format'}), 400
+@app.post('/scan')
+async def scan_image(file: UploadFile = File(...)):
+    if not file:
+        return JSONResponse({'error': 'No file provided'}, status_code=400)
+    
+    filename = file.filename
+    if not allowed_file(filename):
+        return JSONResponse({'error': 'Invalid file format'}, status_code=400)
+    
+    file_ext = filename.rsplit('.', 1)[1].lower()
+    file_name = f'image.{file_ext}'
+    
+    with open(file_name, 'wb') as buffer:
+        contents = await file.read()
+        buffer.write(contents)
+    
+    result = reader.readtext(file_name, detail=0)
+    os.remove(file_name)
+
+   
+    
+    return JSONResponse({'message': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
